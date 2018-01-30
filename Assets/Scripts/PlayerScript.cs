fe3df320b7833f4ca38a0e5f29c111a025e08c82
Layon Martins <layonmartins03@gmail.com>
2018-01-30 16:47:30 -0200

    initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	private float speed = 5f; //velocidade do jogador
+	private int direction; //0 = parado, 1 = direita, -1 = Esquerda
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(direction == 1){
+			//Move o jogador para direita
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		} else if(direction == -1){
+			//Mover para esquerda
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		
+		if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.S))
+			ChangeDirection();
+		
+
+	}
+
+	//muda a direção do jogador
+	void ChangeDirection(){
+		direction = (direction == 1) ? -1 : 1;
+	}
+}
