8afc90e696d8d136482bf52997904572b4695486
Layon Martins <layonmartins03@gmail.com>
2018-02-01 14:29:12 -0200

    add animations

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,19 +6,22 @@ public class PlayerScript : MonoBehaviour {
 
 	private float speed = 5f; //velocidade do jogador
 	private int direction; //0 = parado, 1 = direita, -1 = Esquerda
+	private SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start () {
-		
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(direction == 1){
 			//Move o jogador para direita
+			spriteRenderer.flipX = false;
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		} else if(direction == -1){
 			//Mover para esquerda
+			spriteRenderer.flipX = true;
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
 		
